905b4dec1cbdb58af151e3afe3c62275e3687696
Matthew Fournier <fournier.matthew@gmail.com>
2015-10-03 22:50:10 -0400

    add ansible files
    Issue: convert_to_ansible

--- bin/start_mysql_container.bash
+++
@@ -1,1 +0,0 @@
-docker run --name ocla_mysql --rm -e MYSQL_ROOT_PASSWORD=password mysql/mysql-server:latest
--- bin/start_wordpress_container.bash
+++
@@ -1,1 +0,0 @@
-docker run -it --name ocla_wordpress --link ocla_mysql:mysql --rm -p 8080:80 wordpress
